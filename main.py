3ab452fabc25c6e7490d9bdf874af7f6136e43f7
StephenTaylor1998 <2684109034@qq.com>
2024-06-23 18:04:40 +0800

    update

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 import torch
 
-from amz_cuda_ext.test_neuron.lif import run_eval
+# from amz_cuda_ext.test_neurons.test_if_neuron import test_if_all
+# from amz_cuda_ext.test_neurons.test_lif_neuron import test_lif_all
+from amz_cuda_ext.test_neurons.test_if_neuron import test_if_speed, test_if_error
+from amz_cuda_ext.test_neurons.test_lif_neuron import test_lif_speed, test_lif_error
 
 if __name__ == '__main__':
-    torch.manual_seed(2024)
-    run_eval()
+    # test_if_all()
+    # test_lif_all()
+
+    test_if_error(torch.float32)
+    test_if_speed(torch.float32, n_iter=5000)
+
+    test_if_error(torch.float16)
+    test_if_speed(torch.float16, n_iter=5000)
+
+    test_lif_error(torch.float32)
+    test_lif_speed(torch.float32, n_iter=5000)
+
+    test_lif_error(torch.float16)
+    test_lif_speed(torch.float16, n_iter=5000)
